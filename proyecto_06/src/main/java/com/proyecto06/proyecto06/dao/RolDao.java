96937ddba0bba06bb53f75ce728700e883c5fc4a
Gabriela <mjimenez40125@ufide.ac.cr>
2024-08-06 22:13:20 -0600

    Commit Login de Usuario Gaby
    Interfaces, clases y métodos creados

---
+++ proyecto_06/src/main/java/com/proyecto06/proyecto06/dao/RolDao.java
@@ -0,0 +1,11 @@
+
+package com.proyecto06.proyecto06.dao;
+
+
+import com.proyecto06.proyecto06.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolDao extends JpaRepository<Rol,Long> {
+    
+  
+}
